28ed64d8d48158a27305d8f9f00d56401bfcfdb0
andmatcha <jj.mcr185@gmail.com>
2022-08-16 22:21:36 +0900

    テーブル作成

--- docker/mysql/initdb.d/1_create_tables.sql
+++ docker/mysql/initdb.d/1_create_tables.sql
@@ -10,3 +10,34 @@ CREATE TABLE languages(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS contents;
+
+CREATE TABLE contents(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS study_logs;
+
+CREATE TABLE study_logs(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    study_date DATE NOT NULL,
+    study_time INT NOT NULL
+);
+
+DROP TABLE IF EXISTS study_languages;
+
+CREATE TABLE study_languages(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    study_log_id INT NOT NULL,
+    language_id INT NOT NULL
+);
+
+DROP TABLE IF EXISTS study_contents;
+
+CREATE TABLE study_contents(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    study_log_id INT NOT NULL,
+    language_id INT NOT NULL
+);
